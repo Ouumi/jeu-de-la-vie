f1660be599ff83d68ec9fea56bb05f1985c98167
Ouumi <137509262+Ouumi@users.noreply.github.com>
2024-01-12 21:25:29 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,18 @@ author = 'Graziella'
 release = '0.1'
 version = '0.1.0'
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'final')))
+
 # -- General configuration
 
 extensions = [
